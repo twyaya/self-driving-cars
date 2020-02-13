e799c93034e8b0616497a96e3d13a6c682c2c569
張皓偉 <otis8496@gmail.com>
2020-02-13 12:20:54 +0800

    1

---
+++ index.js
@@ -0,0 +1,257 @@
+		AOS.init();
+		$(".button-collapse").sideNav();
+		
+
+		var wow = new WOW(
+		  {
+			boxClass:     'wow',
+		    callback:     function(box) {
+		      	var roundLogEl = document.querySelector('.round-log');
+		
+				anime({
+				  targets: roundLogEl,
+				  innerHTML: [0, 112],
+				  easing: 'linear',
+				  round: 10 ,
+				  delay: 800,
+				});
+		    },
+		    scrollContainer: null 
+		  }
+		);
+		
+		wow.init();
+		
+		var wow2 = new WOW(
+		  {
+			boxClass:     'wow2',
+		    callback:     function(box) {
+				var audio = $("audio")[1];
+				audio.play();
+		    },
+		    scrollContainer: null 
+		  }
+		);
+		
+		wow2.init();
+		
+		var ctx = document.getElementById("Chart1").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["有", "沒有", "有這個功能但沒使用過", "無法理解問題"],
+				datasets: [{
+					label: '# of Votes',
+					data: [10.7, 78.6, 7.1, 3.6],
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		var ctx = document.getElementById("Chart2").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["有", "沒有", "無法理解問題或不知道"],
+				datasets: [{
+					label: '# of Votes',
+					data: [35.7, 35.7, 28.1],
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		var ctx = document.getElementById("Chart3").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["可以", "不可以"],
+				datasets: [{
+					label: '# of Votes',
+					data: [57.1, 42.9],
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		var ctx = document.getElementById("Chart4").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["是", "否"],
+				datasets: [{
+					label: '# of Votes',
+					data: [21.4, 78.6],
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		var ctx = document.getElementById("Chart5").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["影響大","適當影響", "沒有影響或些許影響"],
+				datasets: [{
+					label: '# of Votes',
+					data: [42.9, 39.3,17.9],
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		var ctx = document.getElementById("Chart6").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["會", "不會"],
+				datasets: [{
+					label: '# of Votes',
+					data: [57.1, 42.9],
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		var ctx = document.getElementById("Chart7").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'pie',
+			data: {
+				labels: ["會", "不會"],
+				datasets: [{
+					label: '# of Votes',
+					data: [60.7, 39.3],
+					backgroundColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)'
+					],
+					borderColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+		
+		$(document).ready(function(){
+		  $("#more").click(function(){
+		    $("#in").toggle();
+		  });
+		  $("#in").hide();
+		});
